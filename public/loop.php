a4a1d669c26e67d4a7e8befb19a4c9dbe5d856cc
Luiz Henrique <campeseluizhenrique@gmail.com>
2025-08-28 23:43:36 +0000

    projeto-1-2025-pt5

---
+++ public/loop.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang "pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Estruturas de Repetição</title>
+</head>
+<body>
+    <ul>
+        <?php
+        $cont = 0;
+        while($cont<=10){
+            echo "<li>" . ($cont * 2) . "</li>";
+            $cont++;
+
+        }
+        ?>
+        </ul>
+        <h1>Do...While</h1>
+        <ul>
+            <?php
+            $cont2 = 11;
+            do{
+                echo "<li>" . ($cont * 2) . "</li>";
+                $cont2++;
+            } while ($cont2 <= 10);
+            ?>
+            </ul>
+            <h1>For</h1>
+            <?php
+            for($cont3 = 0;$cont3 <= 10;$cont3++) {
+                echo "<li>" . ($cont3 * 2) . "</li>";
+            }
+            ?>
+            </ul>
+
+            <h1>Foreach</h1>
+            <?php $frutas = ['Maçã' , 'Banana' , 'Laranja']; ?>
+            <ul>
+                <?php
+                foreach($frutas as $f) {
+                    echo "<li>" . $f . "</li>";
+                }
+                ?>
+                </ul>
+                <h1>Foreach - embedded</h1>
+                <ul>
+                    <?php foreach ($frutas as $f): ?>
+                        <li><?=$f ?></li>
+                        <?php endforeach ?>
+                </ul>
+            </ul>
+    </body>
+    </html>
