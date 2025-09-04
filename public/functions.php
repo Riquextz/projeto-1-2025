0eeb022a83025a08510bad276df221e9402b61e5
Luiz Henrique <campeseluizhenrique@gmail.com>
2025-09-04 23:08:34 +0000

    projeto-1-2025-v5

--- public/functions.php
+++ public/functions.php
@@ -1,1 +1,9 @@
 <?php
+
+function soma($numA, $numB) {
+    return $numA + $numB;
+}
+
+function subtracaoComRetornoPositivo($numA, $numB) {
+    return ($numA > $numB) ? $numA - $numB : $numB - $numA;
+}
