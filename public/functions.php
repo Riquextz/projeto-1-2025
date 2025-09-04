23ca2ed51b49c8c628ce4a4610a9cc26f4d09fbb
Luiz Henrique <campeseluizhenrique@gmail.com>
2025-09-04 23:42:21 +0000

    projeto1-2025-v6

--- public/functions.php
+++ public/functions.php
@@ -7,3 +7,11 @@ function soma($numA, $numB) {
 function subtracaoComRetornoPositivo($numA, $numB) {
     return ($numA > $numB) ? $numA - $numB : $numB - $numA;
 }
+
+function multiplica(int $numA, int $numB) : int {
+    return $numA * numB;
+}
+
+function divide(int $numA, int $numB) : float {
+    return $numA / $numB;
+}
--- public/modelo.php
+++ public/modelo.php
@@ -1,3 +1,6 @@
+<?php
+require_once("./includes/functions.php");
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -6,7 +9,13 @@
     </head>
     <body>
         <?php
-        echo "Teste/n de/n quebra/n de/n linha/n"
+        echo "Teste/n de/n quebra/n de/n linha/n";
+        echo "Teste<br/> de<br/> quebra<br/> de<br/> linha<br/>";
         ?>
+        <p>
+            Soma: <?= soma(30, 20); ?> <br />
+            Subtração: <?= subtracaoComRetornoPositivo(20, 60); ?. <br />
+            Multiplicação: <?= multiplica(2, 5) ?> 
+        </p>
     </body>
 </html>
